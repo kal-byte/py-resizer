00e7304e0633745b5099381143da899fb2b623b8
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 14:16:41 +0100

    First Commit

---
+++ Resizer.py
@@ -0,0 +1,39 @@
+from decouple import config
+from pyimgur import Imgur
+from PIL import Image
+import requests
+
+CLIENT_ID = config("CLIENT_ID")
+imgur = Imgur(CLIENT_ID)
+
+
+def resize_image(image, size):
+    im = Image.open("picture.png")
+    im = im.resize(size, Image.ANTIALIAS)
+    im.save("picture.png")
+
+    upload_image = imgur.upload_image("picture.png")
+
+    return upload_image
+
+
+def resize_url(url, size):
+    go_from_url = url
+
+    with open("picture.png", "wb") as handler:
+        response = requests.get(url, stream=True)
+
+        if not response.ok:
+            print(response)
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handler.write(block)
+
+    im = Image.open("picture.png")
+    im = im.resize(size, Image.ANTIALIAS)
+    im.save("picture.png")
+
+    upload_image = imgur.upload_image("picture.png")
+
+    return upload_image
